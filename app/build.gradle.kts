72f45f349ebce496a2ffd9d9bf362bd6cfb82259
Abish <abishkhanal29@gmail.com>
2025-07-18 15:32:25 +0545

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,14 @@ android {
 
     buildFeatures {
         viewBinding = true
+
+        buildFeatures {
+            compose = true
+            viewBinding = true  // Keep this if it exists
+        }
+        composeOptions {
+            kotlinCompilerExtensionVersion = "1.5.8"
+        }
     }
 
     defaultConfig {
@@ -54,7 +62,15 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
 
+
     implementation ("com.squareup.picasso:picasso:2.71828")
     implementation("com.cloudinary:cloudinary-android:2.1.0")
     implementation("com.squareup.picasso:picasso:2.8")
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
 }
